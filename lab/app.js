a07c017eb1ecb5f92d76b573c359459c07cee26c
fayaz2k <fayazali2k@gmail.com>
2017-03-16 16:42:54 +0500

    Add files via upload

---
+++ lab/app.js
@@ -0,0 +1,77 @@
+/*var per = +prompt("Enter you percetage");
+
+/*document.write("per");
+
+window.alert("per");*
+
+if(per>=90 && per<=100){
+    window.alert("You Got A+ Grade");
+}
+else if(per>=80 && per<=89){
+    window.alert("You Got A Grade");
+}
+else if(per>=70 && per<=79){
+    window.alert("You Got C Grade");
+}
+else if(per>=60 && per<=69){
+    window.alert("You Got D Grade");
+}
+else if(per<50){
+    window.alert("You are Fail");
+}
+else{
+    window.alert("You are Fail");
+}*/
+
+var html = "HTML";
+var java = "java";
+var python = "python";
+
+    /*var a = prompt("Enter HTML Marks");
+    var b = +prompt("Enter Java Marks");*/
+    /*var c = +prompt("Enter python Marks");*/
+
+   /* var score =a;*/
+
+   /* var score = a, b, c;*/
+
+/*if(test === "HTML" && score>=50 && test === "HTML" && score<59 && test === "HTML" && score<59 && java === "java" && score>=50 && python === "python" && score>=50  ){
+    window.alert("Congrates Bro");
+}
+else{
+    window.alert("Fail");
+}
+
+*/
+
+/*if(html === "HTML" && score>=50 || java === "Java" || score>=50 || python === "paython" && score>=50 ){
+    alert("Congrates");
+}
+else{
+    alert("Fail");
+}*/
+
+/*if(html === "HTML" || score === 50){
+    alert("Pass");
+}*/
+
+
+/*var score = 20;
+var name = a;
+
+if(name){
+    alert("login");
+}
+else{
+    alert("not login");
+}*/
+
+    var a = prompt("user Name");
+    var b = +prompt("age");
+    var a = prompt("Where He Teach");
+
+var user = a;
+var age = b;
+var teacher =c;
+var result = (user === "majid" || age === 20 || teacher === "saylani")?   "welcom : Majid is java's teacher" : "Invalid Entry";
+alert(result);
